9dee7fa6fa467556b06a65ddcfbe202f352b9209
ta <tkey.dll@gmail.com>
2025-06-29 19:56:44 +0900

    add console app.

---
+++ apps/NetFwApplication/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Oracle.ManagedDataAccess.Client;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Framework version: " + Environment.Version);
+
+            using (var conn = new OracleConnection("User Id=hr;Password=hr;Data Source=localhost:1521/orclpdb1.localdomain"))
+            {
+                Console.WriteLine(conn.ToString());
+                Console.WriteLine(conn.ConnectionString);
+            }
+
+            Console.WriteLine("Press any key.");
+            Console.ReadLine();
+        }
+    }
+}
